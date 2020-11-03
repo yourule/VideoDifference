385e2bcfb5bfb3c67c1b79e8a3bce9c739fd0dca
Frans-Jan Wind <fjw@yourule.nl>
2020-11-03 22:16:36 +0100

    Initial Commit

---
+++ VideoDifferenceiOS/VideoDifferenceiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoDifferenceiOSApp.swift
+//  VideoDifferenceiOS
+//
+//  Created by Frans-Jan Wind on 03/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct VideoDifferenceiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
